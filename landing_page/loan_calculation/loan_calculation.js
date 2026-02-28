730683d6962d3ad70ae0d7d9a2f9dde0e334173b
MacAbrams <90845816+MacAbrams@users.noreply.github.com>
2026-02-28 11:13:27 -0500

    Update loan_calculation.js
    am I able to commit?

---
+++ landing_page/loan_calculation/loan_calculation.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loan calc</title>
+    <script src="./loan_calculation.js"></script>
+</head>
+<body>
+    <p>
+        testing
+    </p>
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown</button>
+    </div>
+    <label for="principle"></label>
+    <input type="number" id="principle" name="principle">
+    <input type="button" id="submit" name="submit" onclick="submit()">
+</body>
+</html>
